1a4a897450d66878d05ea8ad75ae0ab412f8bc93
Pedro Mezzalira <pedro@stellaefarias.com.br>
2026-09-04 12:45:05 -0300

    commit emergencia

--- resultIA.py
+++ resultIA.py
@@ -2,11 +2,11 @@
 # Não editar manualmente durante a execução do servidor.
 
 RESULTADOS_IA = {'WINV26': {'ativo': 'WINV26',
-            'direcao': 'COMPRA',
-            'forca': 55,
-            'tendencia': 'ALTA',
+            'direcao': 'NEUTRA',
+            'forca': 35,
+            'tendencia': 'LATERAL',
             'volatilidade': 'BAIXA',
             'confianca': 65,
             'risco': 'BAIXO',
-            'estadoMercado': 'FORCA_COMPRADORA_AUMENTANDO',
-            'validoAte': '2026-09-04T12:35:10'}}
+            'estadoMercado': 'LATERAL',
+            'validoAte': '2026-09-04T12:45:28'}}
