31cdb64a075fd80b06f5d2ab9e67128538984131
Pedro Mezzalira <pedro@stellaefarias.com.br>
2026-09-04 12:33:07 -0300

    commit teste

--- resultIA.py
+++ resultIA.py
@@ -3,10 +3,10 @@
 
 RESULTADOS_IA = {'WINV26': {'ativo': 'WINV26',
             'direcao': 'COMPRA',
-            'forca': 65,
+            'forca': 55,
             'tendencia': 'ALTA',
             'volatilidade': 'BAIXA',
-            'confianca': 75,
+            'confianca': 65,
             'risco': 'BAIXO',
             'estadoMercado': 'FORCA_COMPRADORA_AUMENTANDO',
-            'validoAte': '2026-09-04T12:30:04'}}
+            'validoAte': '2026-09-04T12:35:10'}}
